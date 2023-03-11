31deec94444271571d1a0f09d5346f48ed0a9a60
dore96 <Doredelman@gmail.com>
2023-03-11 19:53:47 +0200

    search bar addition

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          dor <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField : ''
+    };
+  }
 
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then((users) => this.setState(() => {
+      return {monsters: users}
+    },
+    () => {console.log(this.state);}
+    ));
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return {searchField}}
+    )};
+
+  render() {
+    const {monsters, searchField} = this.state;
+    const {onSearchChange} = this;
+
+    const filterMonsters = monsters.filter(monster => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return(
+      <div className= 'App'>
+        <input className='searchbox' type = 'search' placeholder="search monsters" onChange={onSearchChange}/>
+        {filterMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
